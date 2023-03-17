031b907867b6c128aedfdbc181b9cb0a9c316d50
Valentina <balendi.art@gmail.com>
2023-03-17 10:25:55 +0100

    Initial Commit

---
+++ Color buttons/Color_buttonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_buttonsApp.swift
+//  Color buttons
+//
+//  Created by Apprenant 18 on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Color_buttonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
