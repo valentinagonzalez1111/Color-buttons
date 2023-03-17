362fcc4345aca97181961d9dff3c93d85aaed0cd
Valentina <balendi.art@gmail.com>
2023-03-17 15:10:28 +0100

    fondo color botón

--- Color buttons/ContentView.swift
+++ Color buttons/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var colorBackground = Color.white
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            colorBackground
+            HStack {
+                SymbolLine(colorFondo: $colorBackground, couleur: .red, texto: "roja")
+                SymbolLine(colorFondo: $colorBackground, couleur: .green, texto: "verde")
+                SymbolLine(colorFondo: $colorBackground, couleur: .blue, texto: "azul")
+            }
+            .padding()
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
+struct SymbolLine: View {
+    
+    @Binding var colorFondo: Color
+    var couleur: Color
+    var texto: String
+   
+    
+    var body: some View {
+        HStack(spacing: 50)  {
+            HStack{
+                Button {
+                    colorFondo = couleur
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .frame(width: 110.0, height: 60.0)
+                            .foregroundColor(couleur)
+                        Label(texto, systemImage: "eyedropper")
+                            .imageScale(.small)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                    }
+                }
+                .foregroundColor(.white)
+                
+            }
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
